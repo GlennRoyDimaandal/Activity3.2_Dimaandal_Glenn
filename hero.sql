d1e6d29cc62d1d32ce74931528cf5a29fc1f66f6
GlennRoyDimaandal <glenndimaandal05@gmail.com>
2023-11-10 14:10:52 +0800

    feat/create-table syntax

--- hero.sql
+++ hero.sql
@@ -1,1 +1,18 @@
 CREATE DATABASE hero;
+
+CREATE TABLE IF NOT EXISTS public.class
+(
+    class_id integer NOT NULL,
+    class_name character varying(255) NOT NULL,
+    class_description text,
+    PRIMARY KEY (class_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.item
+(
+    item_id integer NOT NULL,
+    item_name character varying(255) NOT NULL,
+    item_description text,
+    item_type character varying(50),
+    PRIMARY KEY (item_id)
+);
